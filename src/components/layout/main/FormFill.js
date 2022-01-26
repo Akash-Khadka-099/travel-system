2f3f13281f89fc43bfd6bfeb3505238785571dd1
Akash Khadka <akashkhadka099@gmail.com>
2022-01-26 16:16:37 +0545

    copied inputs of username, email and address and stred as objects in InputDatas using useRef and checked in console and finally worked

--- src/components/layout/main/FormFill.js
+++ src/components/layout/main/FormFill.js
@@ -1,37 +1,64 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import { Form, InputDivs, Input, DivContainer, Button } from "./Form.styled";
 
 function FormFill() {
+
+  const usernameInputRef = useRef();
+  const emailInputRef = useRef();
+  const addressInputRef = useRef();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const enteredUsername = usernameInputRef.current.value;
+    const enteredEmail = emailInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+
+    const InputDatas = {
+      username: enteredUsername,
+      email: enteredEmail,
+      address: enteredAddress,
+    };
+
+    console.log(InputDatas);
+  }
+
   return (
     <>
       <DivContainer>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <InputDivs>
             <label htmlFor="username">Username</label>
             <Input
+              required
               type="text"
               name="username"
               id="username"
               placeholder="Enter username"
+              ref={usernameInputRef}
             />
           </InputDivs>
           <InputDivs>
             <label htmlFor="email">Email</label>
             <Input
+              required
               type="email"
               name="email"
               id="email"
               placeholder="Enter your email"
+              ref={emailInputRef}
             />
           </InputDivs>
           <InputDivs>
             <label htmlFor="address">Address</label>
             <Input
+              required
               type="text"
               name="address"
               id="address"
               placeholder="Enter your address"
+              ref={addressInputRef}
             />
           </InputDivs>
           <InputDivs>
